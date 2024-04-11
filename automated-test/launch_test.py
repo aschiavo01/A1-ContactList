235b56083d787b34fc39f36b09a6dea4946ee573
Alessandro <schiavo.alessandro@protonmail.com>
2024-04-11 16:35:50 +0200

    launch test fix

--- automated-test/launch_test.py
+++ automated-test/launch_test.py
@@ -4,18 +4,18 @@ import time
 # Funzione per eseguire il cherry-pick
 def cherry_pick_and_push(branch_name, commit_hash):
     # Crea il nuovo branch dal branch master
-    os.system(f"git checkout master && git checkout -b {branch_name}")
+    os.system(f"git checkout master && git checkout -b {branch_name}_branch")
     
     # Esegue il cherry-pick della commit
     os.system(f"git cherry-pick -Xtheirs {commit_hash}")
     
     # Effettua la push del branch
-    os.system(f"git push -u origin {branch_name}")
+    os.system(f"git push -u origin {branch_name}_branch")
 
 # Funzione per creare un tag (release)
 def create_tag(branch_name):
-    os.system(f"git tag {branch_name}")
-    os.system(f"git push origin {branch_name}")
+    os.system(f"git tag {branch_name}_branch")
+    os.system(f"git push origin {branch_name}_branch")
 
 start_time = time.time()
 
