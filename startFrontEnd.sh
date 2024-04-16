db804fdebb8171f33c19b478d48a05583b58323e
Alessandro <schiavo.alessandro@protonmail.com>
2024-04-16 18:27:01 +0200

    Auto created commit at 16_04_2024

--- startFrontEnd.sh
+++ startFrontEnd.sh
@@ -1,5 +1,5 @@
 echo "Inizio comandi esecuzione Frontend"
-cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/angular-java-example-master/src/main/ui
+cd /home/runner/work/A1-ContactList/A1-ContactList/insert-here-your-web-app/angular-java-example-master/src/main/ui
 echo "Siamo nella directory FE, proviamo a lanciarlo in esecuzione"
 #npm install
 echo "Installazione npm effettuata, prossimo comando: npm start"
