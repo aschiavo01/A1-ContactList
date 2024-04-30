f76267f88821b4eb1533dfee12962f06be9533f4
Alessandro <schiavo.alessandro@protonmail.com>
2024-04-30 16:14:26 +0200

    Auto created commit at 30_04_2024_16-13-01

--- startBackEnd.sh
+++ startBackEnd.sh
@@ -7,10 +7,10 @@ java -version
 echo "Inizio comandi esecuzione Backend"
 #pwd
 #ls -a
-#cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/angular-java-example-master
+#cd /home/runner/work/A1-ContactList/A1-ContactList/insert-here-your-web-app/angular-java-example-master
 #ls -a
 #mvn clean install
-cd /home/runner/work/HookTestRepo/HookTestRepo/insert-here-your-web-app/angular-java-example-master/target
+cd /home/runner/work/A1-ContactList/A1-ContactList/insert-here-your-web-app/angular-java-example-master/target
 echo "Vediamo quali file jar si trovano in cartella target"
 ls -a
 java -jar users-0.0.1-SNAPSHOT.jar &
