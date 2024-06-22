d359ed5225d198374b6a897d73b7b0380a9c75f6
Alessandro <schiavo.alessandro@protonmail.com>
2024-06-22 10:22:37 +0200

    updated setup-fork to get latest version o locators automated tests

--- setup-fork.sh
+++ setup-fork.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# To obtain the tags to be used
+# To retrieve latest version of locators-automated-tests
+git submodule update --init --recursive
 
+# To obtain the tags to be used
 git remote add upstream https://github.com/ares-17/A1-ContactList.git
 git fetch upstream --tags
 git push origin --tags 
