fd0adabcb9a0f0a1f7744a9eb8af268d2e9e1844
Alessandro <schiavo.alessandro@protonmail.com>
2024-06-22 11:56:26 +0200

    set original upstream

--- setup-fork.sh
+++ setup-fork.sh
@@ -5,5 +5,6 @@ git submodule update --init --recursive
 
 # To obtain the tags to be used
 git remote add unina-upstream https://github.com/reverse-unina/A1-ContactList.git
+git remote add upstream https://github.com/ares-17/A1-ContactList.git
 git fetch unina-upstream --tags
 git push origin --tags 
